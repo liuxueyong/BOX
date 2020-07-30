e5a62b9efca2026d71690b2873f2596716d3eed5
LXY <liu.xueyong@qq.com>
2020-07-30 13:39:13 +0800

    update

---
+++ js/loadbar.js
@@ -0,0 +1,27 @@
+ function LoadingBar(id)
+        {
+            this.loadbar = $("#" + id);
+            this.percentEle = $(".percent", this.loadbar);
+            this.percentNumEle = $(".percentNum", this.loadbar);
+            this.max = 100;
+            this.currentProgress = 0;
+        }
+        LoadingBar.prototype = {
+            constructor: LoadingBar,
+            setMax: function (maxVal)
+            {
+                this.max = maxVal;
+            },
+            setProgress: function (val)
+            {
+                if (val >= this.max)
+                {
+                    val = this.max;
+                }
+                this.currentProgress = parseInt((val / this.max) * 100) + "%";
+                this.percentEle.width(this.currentProgress);
+                this.percentNumEle.text(this.currentProgress);
+ 
+ 
+            }
+        };
